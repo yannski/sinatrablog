244cfe0913b9d845806de1c1e88ed9b338588387
Yann Klis <yann.klis@novelys.com>
2015-04-21 10:50:07 +0200

    layout

--- app.rb
+++ app.rb
@@ -2,20 +2,29 @@ require 'sinatra'
 require 'haml'
 
 class Article
-  attr_accessor :title, :body
+  attr_accessor :title, :body, :number
+
+  def self.article_from_file i
+    lines = File.readlines("articles/#{i}.txt")
+    title = lines.shift
+    body = lines.join("\n")
+    article = Article.new
+    article.title = title
+    article.body = body
+    article.number = i
+    article
+  end
 
   def self.load_all
     numbers = [1, 2, 3, 4, 5, 6, 7, 8]
     articles_array = numbers.reverse.map{|i|
-      lines = File.readlines("articles/#{i}.txt")
-      title = lines.shift
-      body = lines.join("\n")
-      article = Article.new
-      article.title = title
-      article.body = body
-      article
+      article = article_from_file(i)
     }
     return articles_array
+  end
+
+  def self.load i
+    article_from_file(i)
   end
 end
 
@@ -23,4 +32,10 @@ get '/' do
   @articles = Article.load_all
 
   haml :index
+end
+
+get '/articles/:number' do
+  @article = Article.load( params[:number] )
+
+  haml :show
 end
